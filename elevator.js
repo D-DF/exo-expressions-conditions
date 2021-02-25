6fca899559af2c9d897b05722a90b9c3a065dd36
D-DF <daviddefreitas0192@gmail.com>
2021-02-25 17:04:42 +0100

    first commit

---
+++ elevator.js
@@ -0,0 +1,43 @@
+let currentFloor = -2;
+let targetFloor = 7;
+let user = 2;
+
+const ascenceur = () => {
+
+  
+
+}
+
+switch(currentFloor) {
+  case 7: 
+    console.log("Vous êtes à l'étage à 7");
+    break;
+  case 6:
+    console.log("Vous êtes à l'étage 6");
+    break;
+  case 5:
+    console.log("Vous êtes à l'étage 5");
+    break;
+  case 4:
+    console.log("Vous êtes à l'étage 4");
+    break;
+  case 3: 
+    console.log("Vous êtes à l'étage 3");
+    break;
+  case 2:
+    console.log("Vous êtes à l'étage 2");
+    break;
+  case 1:
+    console.log("Vous êtes à l'étage 1");
+    break;
+  case 0:
+    console.log("Vous êtes au rez de chaussée");
+    break;
+  case -1:
+    console.log("Vous êtes au -1");
+    break;
+  case -2:
+    console.log("Vous êtes au -2");
+    break;
+}
+
---
+++ welcome.js
@@ -0,0 +1,3 @@
+let name = "David";
+let gender = "Homme".toLowerCase();
+console.log(gender === "homme" ? `Bonjour ${name}, vous êtes entré sur le chan` : `Bonjour ${name}, vous êtes entrée sur le chan`);
